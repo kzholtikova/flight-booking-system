27a81aca51d916820be1518ce5d480801083877e
Kira Zholtikova <kzholtikova@kse.org.ua>
2024-09-10 12:29:32 +0300

    flight info validation

--- src/fileReader.cpp
+++ src/fileReader.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <fstream>
 #include <sstream>
 #include "../include/fileReader.h"
@@ -21,4 +20,28 @@ void FileReader::readConfig(const std::string& path, System& system) {
 
         system.addAirplane(airplane);
     }
+}
+
+void FileReader::readSeatsPrices(std::stringstream &ss, Airplane &airplane) {
+    int numberOfSeats, price;
+    while (ss >> numberOfSeats >> price) {
+        if (ss.fail())
+            throw std::invalid_argument("Invalid seats prices data for " + airplane.getFlightNumber());
+        airplane.addSeats(numberOfSeats, price);
+    }
+
+    if (ss.fail())
+        throw std::invalid_argument("The price for seats is missing for " + airplane.getFlightNumber());
+}
+
+void FileReader::validateFlightInfo(std::tm& tm, const std::string& flightDate, const std::string& flightNo, const std::string& seatsPerRow) {
+    std::stringstream ss(flightDate);
+    ss >> std::get_time(&tm, "%d.%m.%Y");
+
+    if (ss.fail() || ss.eof())
+        throw std::invalid_argument("Invalid flight date for " + flightNo);
+    if (flightNo.length() < 3 || !isalpha(flightNo[0]) || !isalpha(flightNo[1]) || !std::all_of(flightNo.begin() + 2, flightNo.end(), ::isdigit))
+        throw std::invalid_argument("Invalid flight number for " + flightNo);
+    if (seatsPerRow.empty() || !std::all_of(seatsPerRow.begin(), seatsPerRow.end(), ::isdigit) || std::stoi(seatsPerRow) <= 0)
+        throw std::invalid_argument("Invalid seats per row number for " + flightNo);
 }
